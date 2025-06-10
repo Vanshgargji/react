33e09c9b8f2dfdc0e04594c1403c13135ed23a50
Vansh Garg <vansgarg55@gmail.com>
2025-06-11 01:01:46 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,37 @@
+// const heading = React.createElement(
+//   "h1" , {id: "heading"} , "Hello world from React"
+// );
+
+// console.log(heading);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+//    <div id="parent">
+//   <div id="child1">
+//     <h1>i am h1</h1>
+//     <h2>i am h1</h2>
+//   </div>
+//   <div id="child2">
+//     <h1>i am h1</h1>
+//     <h2>i am h1</h2>
+//   </div>
+//  </div>
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "i am h1"),
+    React.createElement("h2", {}, "i am h2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "i am h1"),
+    React.createElement("h2", {}, "i am h2"),
+  ]),
+]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+// heading is an object
+// when root.render is used it extract the h1 tag from the object and then render it in the root
