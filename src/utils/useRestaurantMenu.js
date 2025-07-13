5e8e8d9a3f2c18e1889dae7cd2077a4f51f7a97e
Vansh Garg <vansgarg55@gmail.com>
2025-07-14 01:55:23 +0530

    backend connected

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -8,7 +8,7 @@ const useRestaurantMenu = (resId) => {
   }, []);
 
   const fetchMenuItems = async () => {
-    const data = await fetch( `https://server-swiggy.onrender.com//api/menu?resId=${resId}`);
+    const data = await fetch( `https://server-swiggy.onrender.com/api/menu?resId=${resId}`);
 
     const json = await data.json();
     console.log(json);
